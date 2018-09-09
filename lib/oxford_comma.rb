9843d4ee7795c0a1cfcee5bd5ac9a055643856d3
patrickp246@gmail.com <patrickp246@gmail.com>
2018-09-09 15:53:53 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array[0]
+  elsif array.count == 2
+    array[0] + " and " + array[1]
+  elsif array.count == 3
+    "#{array[0]}, #{array[1]} and #{array[2]}"
+  else
+    
+  end
 end
